74c2a6c416b0107fbbf55a71de74f195fd56b794
Nathan Cai <wenjuncai97@gmail.com>
2022-02-11 02:30:22 -0500

    final spring challenge

--- index.js
+++ index.js
@@ -264,13 +264,10 @@ Use removeArtist to do the following:
 For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
 function removeArtist(array, index) {
-  for(let i = 0; i <array.length; i++){
-  if(array[i] === index){
-  array.splice(i, 1);
+  array.splice(index, 1);
+  return array.length;
   }
-}
-return array.length -1;
-}
+  
 console.log(removeArtist(artists, 0));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
